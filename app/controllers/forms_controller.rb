c76300072c1336b2254a747f4835b920110f6617
mathias loh <matlohck@gmail.com>
2023-06-23 13:43:04 +0800

    temp

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,5 @@
+class FormsController < ApplicationController
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: %i[new create index]
-
-  resources :campaigns do
-    resources :form, only: %i[new create]
-  end
   resources :leads
 
-    # resources :forms, only: %i[index new create show]
-  # end
+  resources :campaigns do
+    resources :forms, only: %i[new create]
+  end
+
   get "dashboard", to: "pages#dashboard"
 end
