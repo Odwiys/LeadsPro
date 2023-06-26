2e038362dcc114d8d0f32cc2c373070209f96fb6
Loges Manivannan <antlersandladder@gmail.com>
2023-06-26 10:11:41 +0800

    Added navbar aand made changes to the views when user has loggedout and logged in

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
+
+import SearchCampaignsController from "./search_campaigns_controller"
+application.register("search-campaigns", SearchCampaignsController)
