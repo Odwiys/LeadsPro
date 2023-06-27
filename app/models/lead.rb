eec29b1f29b348f582a3ed3e6ab22b806ada9a53
mathias loh <matlohck@gmail.com>
2023-06-27 21:39:12 +0800

    sentiment analysis

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -8,21 +8,25 @@ class Lead < ApplicationRecord
 
   after_create :analyse_sentiment
 
-  def analyse_sentiment
-    # response.answers
-    # - answer.sentiment
+  def analyse_sentiment(response)
+    answers = response.answers
+    rating = 0.0
+    answers.each do |answer|
+      value = answer.sentiment_value
+      rating += value
+      # if multiple choice, take hard coded sentiment points
+        # rating += answer.sentiment
+      # else, run chatgpt analysis
+        # rating += answer.sentiment
+      # end
+    end
+    # # calculate the average / total rating
+    final_rating = rating / anwers.length
+    # rating = Float
+    # attribute rating to leads > rating
+    this.rating = final_rating
+
     # self.rating = 2
     # save!
-
-    # rating = 0.0
-    # response.answers.each do |answer|
-    #   rating += answer.sentiment
-    # end
-    # # calculate the average / total rating
-    # rating
-    # # return <Float
-
-    # if multiple choice, hard code sentiment points
-    # else, run chatgpt analysis
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_011240) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_082833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_011240) do
     t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "sentiment_value"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["response_id"], name: "index_answers_on_response_id"
   end
