e252ec409c8a65e329910f0743eed5eeadf76629
Loges Manivannan <antlersandladder@gmail.com>
2023-06-28 15:22:12 +0800

    Made changes to the campaign form, working on the partials still

---
+++ app/javascript/controllers/question_form_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["options"];
+
+  connect() {
+    this.toggleOptionFields();
+  }
+
+  toggleOptionFields() {
+    const formatValue = this.element.value;
+    const optionsTarget = this.optionsTarget;
+
+    if (formatValue === "Multiple-Choice") {
+      optionsTarget.classList.remove("d-none");
+    } else {
+      optionsTarget.classList.add("d-none");
+    }
+  }
+}
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -6,23 +6,23 @@ class Lead < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true, uniqueness: true
 
-  after_create :analyse_sentiment
+  # after_create :analyse_sentiment
 
-  def analyse_sentiment
-    # response.answers
-    # - answer.sentiment
-    # self.rating = 2
-    # save!
+  # def analyse_sentiment
+  #   # response.answers
+  #   # - answer.sentiment
+  #   # self.rating = 2
+  #   # save!
 
-    # rating = 0.0
-    # response.answers.each do |answer|
-    #   rating += answer.sentiment
-    # end
-    # # calculate the average / total rating
-    # rating
-    # # return <Float
+  #   # rating = 0.0
+  #   # response.answers.each do |answer|
+  #   #   rating += answer.sentiment
+  #   # end
+  #   # # calculate the average / total rating
+  #   # rating
+  #   # # return <Float
 
-    # if multiple choice, hard code sentiment points
-    # else, run chatgpt analysis
-  end
+  #   # if multiple choice, hard code sentiment points
+  #   # else, run chatgpt analysis
+  # end
 end
